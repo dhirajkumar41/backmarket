b68f0584fbf92a4a4f8c7b8fadbf83611c536280
shrutika <shrutika.gaikwad@ranosys.com>
2021-02-04 12:53:12 +0530

    test

--- Test/Cron/Test.php
+++ Test/Cron/Test.php
@@ -7,11 +7,11 @@ use \Psr\Log\LoggerInterface;
 class Test {
 
   protected $logger;
+  protected $curl;
 
   public function __construct(LoggerInterface $logger, \Magento\Framework\HTTP\Client\Curl $curl) {
-
-    $this->logger = $logger;
     $this->curl = $curl;
+    $this->logger = $logger;
   }
 
   /**
@@ -25,8 +25,16 @@ class Test {
   */
 
   public function execute() {
-  
-        $this->logger->info("hiiiii");
+        $url='https://www.backmarket.com/ws/orders';        
+        $params = '{
+                    Content-type: application/json",
+                    Accept: application/json
+                    Authorization: Basic YWNjb3VudHNAY29ldGVjaGluYy5jb206SmFpbWF0YWRpNzkw
+                   }';
+        $this->_curl->get($url, $params);
+        $response = $this->_curl->getBody();
+        $response=json_decode($response,true);
+        $this->logger->info($response);
 
   }
 
